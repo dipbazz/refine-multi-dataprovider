572012dc35ea12f2d6969eaeaf4cff71e522db53
Dipesh Bajgain <dipbazz@gmail.com>
2022-12-25 12:49:55 +0545

    Add two data provider to display posts

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,37 @@ import { Refine } from "@pankod/refine-core";
 
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
+import 'index.css';
+
+import { PostList } from "pages/refine/posts";
+import { Layout } from "components/Layout";
+import { JsonPostList } from "pages/json/posts";
 
 function App() {
   return (
     <Refine
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      dataProvider={{
+        default: dataProvider("https://api.fake-rest.refine.dev"),
+        jsonPlaceholder: dataProvider("https://jsonplaceholder.typicode.com")
+      }}
       routerProvider={routerProvider}
+      Layout={Layout}
+      resources={[
+        {
+          name: 'posts',
+          list: PostList,
+          options: {
+            label: 'Refine posts'
+          }
+        },
+        {
+          name: 'json',
+          list: JsonPostList,
+          options: {
+            label: 'JSON placeholder posts'
+          }
+        }
+      ]}
     />
   );
 }
---
+++ src/pages/refine/posts/list.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import { useTable, ColumnDef, flexRender } from "@pankod/refine-react-table";
+import {
+    AiOutlineLeft,
+    AiOutlineRight,
+    AiOutlineDoubleLeft,
+    AiOutlineDoubleRight
+} from 'react-icons/ai';
+
+import { IPost } from "interfaces";
+
+export const PostList: React.FC = () => {
+    const columns = React.useMemo<ColumnDef<IPost>[]>(
+        () => [
+            {
+                id: "id",
+                header: "ID",
+                accessorKey: "id",
+            },
+            {
+                id: "title",
+                header: "Title",
+                accessorKey: "title",
+            },
+            {
+                id: "status",
+                header: "Status",
+                accessorKey: "status",
+            },
+            {
+                id: "createdAt",
+                header: "CreatedAt",
+                accessorKey: "createdAt",
+            },
+        ],
+        [],
+    );
+
+    const {
+        getState,
+        setPageIndex,
+        getCanPreviousPage,
+        getPageCount,
+        getCanNextPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        getHeaderGroups,
+        getRowModel
+    } = useTable<IPost>({
+        columns
+    });
+
+    return (
+        <div className="container mx-auto pb-4">
+            <table className="min-w-full table-fixed divide-y divide-gray-200 border">
+                <thead className="bg-gray-100">
+                    {getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th
+                                    key={header.id}
+                                    colSpan={header.colSpan}
+                                    className="py-3 px-6 text-left text-xs font-medium uppercase tracking-wider text-gray-700 "
+                                >
+                                    {flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext(),
+                                    )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                    {getRowModel().rows.map((row) => {
+                        return (
+                            <tr
+                                key={row.id}
+                                className="transition hover:bg-gray-100"
+                            >
+                                {row.getVisibleCells().map((cell) => {
+                                    return (
+                                        <td
+                                            key={cell.id}
+                                            className="whitespace-nowrap py-2 px-6 text-sm font-medium text-gray-900"
+                                        >
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext(),
+                                            )}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <div className="mt-2 flex items-center justify-end gap-4">
+                <div className="flex gap-1">
+                    <button
+                        onClick={() => setPageIndex(0)}
+                        disabled={!getCanPreviousPage()}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white disabled:bg-gray-200 hover:disabled:text-black"
+                    >
+                        <AiOutlineDoubleLeft />
+                    </button>
+                    <button
+                        onClick={() => previousPage()}
+                        disabled={!getCanPreviousPage()}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white disabled:bg-gray-200 hover:disabled:text-black"
+                    >
+                        <AiOutlineLeft />
+                    </button>
+                    <button
+                        onClick={() => nextPage()}
+                        disabled={!getCanNextPage()}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white disabled:bg-gray-200 hover:disabled:text-black"
+                    >
+                        <AiOutlineRight />
+                    </button>
+                    <button
+                        onClick={() => setPageIndex(getPageCount() - 1)}
+                        disabled={!getCanNextPage()}
+                        className="flex items-center justify-between gap-1 rounded border border-gray-200 p-2 text-xs font-medium leading-tight transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white disabled:bg-gray-200 hover:disabled:text-black"
+                    >
+                        <AiOutlineDoubleRight />
+                    </button>
+                </div>
+                <span>
+                    Page
+                    <strong>
+                        {getState().pagination.pageIndex + 1} of{" "}
+                        {getPageCount()}
+                    </strong>
+                </span>
+                <span>
+                    Go to page:
+                    <input
+                        type="number"
+                        defaultValue={getState().pagination.pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0;
+                            setPageIndex(page);
+                        }}
+                        className="w-12 rounded border border-gray-200 p-1 text-gray-700"
+                    />
+                </span>
+                <select
+                    value={getState().pagination.pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                    }}
+                    className="rounded border border-gray-200 p-1 text-gray-700"
+                >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+};
